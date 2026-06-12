659fe1149d7326da2bd5597eb9c1478b9bd892d5
lhl0817-ui <lhl0817@gmail.com>
2026-06-12 23:13:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -168,3 +168,56 @@ accordionBtns.forEach(btn => {
     });
 
 });
+const weddingDate =
+new Date("2026-10-10T12:00:00");
+
+function updateCountdown(){
+
+    const now = new Date();
+
+    const diff =
+    weddingDate - now;
+
+    const days =
+    Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    const hours =
+    Math.floor(
+        (diff % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes =
+    Math.floor(
+        (diff % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds =
+    Math.floor(
+        (diff % (1000 * 60))
+        / 1000
+    );
+
+   document.getElementById("days")
+.textContent = days;
+
+document.getElementById("hours")
+.textContent =
+String(hours).padStart(2,"0");
+
+document.getElementById("minutes")
+.textContent =
+String(minutes).padStart(2,"0");
+
+document.getElementById("seconds")
+.textContent =
+String(seconds).padStart(2,"0");
+}
+
+updateCountdown();
+
+setInterval(
+    updateCountdown,
+    1000
+);
